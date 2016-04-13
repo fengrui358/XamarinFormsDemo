443afc6d52431fac80e28775db913adfd00f970a
�ؽ� <qjdev@qq.com>
2016-04-13 23:39:10 +0800

    跳转到搜索建议页面

--- XamarinFormsDemo/XamarinFormsDemo/Views/BaiduMapView.xaml.cs
+++ XamarinFormsDemo/XamarinFormsDemo/Views/BaiduMapView.xaml.cs
@@ -3,16 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using GalaSoft.MvvmLight.Ioc;
 using Xamarin.Forms;
+using XamarinFormsDemo.Hybrid;
 
 namespace XamarinFormsDemo.Views
 {
     public partial class BaiduMapView : ContentPage
     {
+        private RelativeLayout _relativeLayout;
+        private Entry _searchEntry;
+
         public BaiduMapView()
         {
             InitializeComponent();
+
+            //todo:使用xaml构建该页面
+            _relativeLayout = new RelativeLayout
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var hybirdWeb = new HybridWebView
+            {
+                Uri = "index.html",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            _relativeLayout.Children.Add(hybirdWeb,
+                        Constraint.RelativeToParent((parent) => { return parent.X; }),
+                        Constraint.RelativeToParent((parent) => { return parent.Y; }),
+                        Constraint.RelativeToParent((parent) => { return parent.Width; }),
+                        Constraint.RelativeToParent((parent) => { return parent.Height; })
+                    );
+
+            var _searchEntry = new Entry{HorizontalOptions = LayoutOptions.FillAndExpand, BackgroundColor = Color.White, Placeholder = "搜索", TextColor = Color.Gray};
+            _searchEntry.Focused += SearchEntryOnFocused;
+
+            _relativeLayout.Children.Add(_searchEntry, Constraint.RelativeToParent(parent => parent.X + 20),
+                Constraint.RelativeToParent(parent => parent.Y + 35),
+                Constraint.RelativeToParent(parent => parent.Width - 40), Constraint.Constant(45));
+
+            Content = _relativeLayout;
+        }
+
+        private async void SearchEntryOnFocused(object sender, FocusEventArgs focusEventArgs)
+        {
+            var keyWords = ((Entry) sender).Text;
+
+            var navigationPage = SimpleIoc.Default.GetInstance<NavigationPage>(typeof(MainPageView).ToString());
+            await navigationPage.PushAsync(new SearchSuggestView(keyWords));
         }
     }
 }
---
+++ XamarinFormsDemo/XamarinFormsDemo/Views/SearchSuggestView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsDemo.Views
+{
+    public partial class SearchSuggestView : ContentPage
+    {
+        private string _keyWords;
+
+        public SearchSuggestView(string keyWords)
+        {
+            InitializeComponent();
+
+            _keyWords = keyWords;
+        }
+    }
+}
