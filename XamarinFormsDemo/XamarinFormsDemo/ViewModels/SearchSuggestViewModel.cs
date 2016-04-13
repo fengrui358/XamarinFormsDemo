78b7b25a371f82d16d1939f4d128effea155a8a2
�ؽ� <qjdev@qq.com>
2016-04-14 00:10:19 +0800

    准备使用百度的搜索建议API

---
+++ XamarinFormsDemo/XamarinFormsDemo/ViewModels/SearchSuggestViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace XamarinFormsDemo.ViewModels
+{
+    public class SearchSuggestViewModel : ViewModelBase
+    {
+        private string _searchKeyWords;
+        private List<string> _suggestResults; 
+
+        public string SearchKeyWords
+        {
+            get { return _searchKeyWords; }
+            set
+            {
+                var x = value;
+                Set(() => SearchKeyWords, ref _searchKeyWords, value);
+            }
+        }
+
+        /// <summary>
+        /// 用整组替换的方式，效率好些
+        /// </summary>
+        public List<string> SuggestResults
+        {
+            get { return _suggestResults; }
+            set { Set(() => SuggestResults, ref _suggestResults, value); }
+        } 
+
+        public SearchSuggestViewModel(string keyWords)
+        {
+            _suggestResults = new List<string>();
+            SearchKeyWords = keyWords;
+            TextChangedHandler(keyWords);
+        }
+
+        public void TextChangedHandler(string newKeyWord)
+        {
+            if (string.IsNullOrEmpty(newKeyWord))
+            {
+                _suggestResults = new List<string>();
+            }
+
+
+        }
+    }
+}
--- XamarinFormsDemo/XamarinFormsDemo/Views/SearchSuggestView.xaml.cs
+++ XamarinFormsDemo/XamarinFormsDemo/Views/SearchSuggestView.xaml.cs
@@ -5,18 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using XamarinFormsDemo.ViewModels;
 
 namespace XamarinFormsDemo.Views
 {
     public partial class SearchSuggestView : ContentPage
     {
-        private string _keyWords;
+        private SearchSuggestViewModel _viewModel;
 
         public SearchSuggestView(string keyWords)
         {
             InitializeComponent();
 
-            _keyWords = keyWords;
+            _viewModel = new SearchSuggestViewModel(keyWords);
+            BindingContext = _viewModel;
+        }
+
+        private void Entry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            _viewModel.TextChangedHandler(e.NewTextValue);
         }
     }
 }
